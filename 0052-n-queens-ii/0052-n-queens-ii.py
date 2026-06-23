eb1ca596a51ef22fd54edcf06303d1c5ccfc947e
NagaRani5 <naga07391@gmail.com>
2026-06-23 14:12:10 +0530

    Time: 11 ms (43.22%), Space: 19.9 MB (6.85%) - LeetHub

---
+++ 0052-n-queens-ii/0052-n-queens-ii.py
@@ -0,0 +1,25 @@
+class Solution:
+    def totalNQueens(self, n: int) -> int:
+        board=[["."]*n for _ in range(n)]
+        res=[]
+        cols=set()
+        dig1=set()
+        dig2=set()
+        def bt(row):
+            if row==n:
+                res.append(["".join(row) for row in board])
+            for col in range(n):
+                if col in cols or (row-col) in dig1 or (row+col) in dig2:
+                    continue
+                board[row][col]="Q"
+                cols.add(col)
+                dig1.add(row-col)
+                dig2.add(row+col)
+                bt(row+1)
+                board[row][col]="."
+                cols.remove(col)
+                dig1.remove(row-col)
+                dig2.remove(row+col)
+        bt(0)
+        return len(res)
+        
